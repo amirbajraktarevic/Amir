825659fd7dd85094e86f58e72cfb3d227eed13fc
Amir Bajraktarevic <Amir@Amirs-MacBook-Pro.local>
2020-10-26 13:32:48 +0100

    Fixed some things

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,14 +2,27 @@ import React from "react";
 import "../App.css";
 
 class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: " ",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ value: e.target.value });
+  }
+
   render() {
     return (
       <div className="contact">
         <div className="container">
-            <br/>
-            <br/>
-            <br/>
-            <br/>
+          <br />
+          <br />
+          <br />
+          <br />
           <div className="d-flex justify-content-center">
             <h1>Get in contact!</h1>
           </div>
@@ -17,6 +30,8 @@ class Contact extends React.Component {
             <form>
               <h4>Your name and surname:</h4>
               <input
+                value={this.state.value}
+                onChange={this.handleChange}
                 className="col-md-12"
                 type="text"
                 placeholder="Your name."
@@ -24,6 +39,9 @@ class Contact extends React.Component {
               ></input>
               <br />
               <br />
+              <h4>Your e-mail:</h4>
+              <input className="mb-4 col-md-12" type="text"/>
+              <br/>
               <h4>Your question/proposal:</h4>
               <textarea
                 className="col-md-12"
@@ -36,25 +54,30 @@ class Contact extends React.Component {
             </form>
           </div>
 
-            <div className="mt-5 d-flex justify-content-center">
-            
+          <div className="mt-5 d-flex justify-content-center">
+            <span>Thank you for your feedback {this.state.value} !</span>
+          </div>
+
+          <div className="mt-5 d-flex justify-content-center">
             <h5>Socials:</h5>
             <div className="spacing">
-            <a href="https://www.facebook.com/amir.bajraktarevic.69/" rel="noopner noreferrer">
-            <i class='fab fa-facebook'></i>
-            </a>
-            <a href="https://www.instagram.com/amir_bajraktarevic/">
-            <i class='fab fa-instagram'></i>
-            </a>
-            <a href="https://github.com/amirbajraktarevic">
-            <i class='fab fa-github'></i>   
-            </a>
+              <a
+                href="https://www.facebook.com/amir.bajraktarevic.69/"
+                rel="noopner noreferrer"
+              >
+                <i class="fab fa-facebook"></i>
+              </a>
+              <a href="https://www.instagram.com/amir_bajraktarevic/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://github.com/amirbajraktarevic">
+                <i class="fab fa-github"></i>
+              </a>
             </div>
-            </div>
-
+          </div>
         </div>
         <footer className="mt-5 footer-copyright text-center py-3">
-            &#169; 2020 Copyright: Amir Bajraktarevic
+          &#169; 2020 Copyright: Amir Bajraktarevic
         </footer>
       </div>
     );
